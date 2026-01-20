3a12bd7dae2e0094ea39257f0e235affb8d02280
Mana-scripts <madsbrriinckbastholm@gmail.com>
2026-01-20 17:37:46 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,18 @@
-local function LoadScript(ID)
-    local success, err = pcall(function()
-        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Mana-scripts/Neverlose-UI/refs/heads/main/Games/"..tostring(ID)..".lua"))()
-    end)
-    if not success then
-        warn("Something went wrong | ", err)
+local function LoadScript(id)
+    local LoadScript_Table = {}
+
+    function LoadScript_Table:Load(str)
+        local success, err = pcall(function()
+            loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Mana-scripts/Neverlose-UI/refs/heads/main/Games/"..tostring(id).."/"..LoadScript_Table.Source..".lua"))()
+        end)
+        if not success then
+            warn("Something went wrong | ", err)
+        end
     end
+
+    return LoadScript_Table
 end
 
-LoadScript(game.PlaceId)
+local Loader = LoadScript(game.PlaceId)
+--Loader.Source = "Source"
+--Loader:Load()
