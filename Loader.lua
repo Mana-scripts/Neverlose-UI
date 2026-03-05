1d4b3038d47143b10fee4779cc4adbea68dea7ca
Mana-scripts <madsbrriinckbastholm@gmail.com>
2026-03-05 14:26:56 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,16 +1,40 @@
-local Loader_Module = {
-    Source = ""
+local Data = {
+	Owner = "Mana",
+	Library = "Neverlose-UI"
 }
 
-function Loader_Module:LoadScript(id)
+local BaseURL = ("https://raw.githubusercontent.com/%s-scripts/%s/refs/heads/main/Games/"):format(Data.Owner, Data.Library)
 
-    local success, err = pcall(function()
-        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Mana-scripts/Neverlose-UI/refs/heads/main/Games/"..tostring(id).."/"..Loader_Module.Source..".lua"))()
-    end)
-    if not success then
-        warn("Something went wrong | ", err)
-    end
+local function EncodeSpaces(str)
+	return BaseURL .. str:gsub(" ", "%%20")
+end
+
+local Games = {
+	[18172550962] = EncodeSpaces("Pixel Blade.lua"),
+	[18172553902] = EncodeSpaces("Pixel Blade.lua"),
+	[130247632398296] = EncodeSpaces("Anime Fighting Simulator.lua"),
+	[76285745979410] = EncodeSpaces("Anime Card Collection.lua"),
+	[75992362647444] = EncodeSpaces("Tap Simulator.lua"),
+	[74260430392611] = EncodeSpaces("Rebirth Champions Ultimate.lua"),
+}
+
+function LoadScript(id)
+	local url = Games[id]
+
+	if not url then
+		warn("Game not supported:", id)
+		return
+	end
+
+	print("Loading:", url)
+
+	local scriptSource = game:HttpGetAsync(url)
+	loadstring(scriptSource)()
+	print("Welcome "..game.Players.LocalPlayer.Name.."!")
 
 end
 
-return Loader_Module
+LoadScript(game.PlaceId)
+
+print(game.PlaceId)
+setclipboard(tostring(game.PlaceId))
