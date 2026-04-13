2560f180a4d9110221537ef0d91f4792e1539777
Mana-scripts <madsbrriinckbastholm@gmail.com>
2026-04-13 20:12:57 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -18,18 +18,52 @@ local function EncodeSpaces(str)
 end
 
 local Games = {
+	-- [[Pixel Blade]] --
 	[18172550962] = EncodeSpaces("Pixel Blade.lua"), -- Spawn
 	[18172553902] = EncodeSpaces("Pixel Blade.lua"),
 	[133884972346775] = EncodeSpaces("Pixel Blade.lua"),
+
+	-- [[Anime Fighting Simulator]] --
 	[130247632398296] = EncodeSpaces("Anime Fighting Simulator.lua"),
+
+	-- [[Anime Card Collection]] --
 	[76285745979410] = EncodeSpaces("Anime Card Collection.lua"),
+
+	-- [[Tap Simulator]] --
 	[75992362647444] = EncodeSpaces("Tap Simulator.lua"),
 	[111187356770616] = EncodeSpaces("Tap Simulator.lua"), 
+
+	-- [[Rebirth Champions Ultimate]] --
 	[74260430392611] = EncodeSpaces("Rebirth Champions Ultimate.lua"),
+
+	-- [[Blox Fruits]] --
 	[2753915549] = EncodeSpaces("Blox Fruits.lua"), -- World 1
 	[4442272183] = EncodeSpaces("Blox Fruits.lua"), -- World 2
 	[7449423635] = EncodeSpaces("Blox Fruits.lua"), -- World 3
+
+	-- [[Anime Final Quest]] --
+	[70768610276749] = EncodeSpaces("Anime Final Quest.lua"), -- Wave3
+	[73019180051115] = EncodeSpaces("Anime Final Quest.lua"), -- Endless1
+	[73912821025828] = EncodeSpaces("Anime Final Quest.lua"), -- Raid2
+	[77367808280910] = EncodeSpaces("Anime Final Quest.lua"), -- DivineGeneral
+	[81100481912247] = EncodeSpaces("Anime Final Quest.lua"), -- Cracker Land
+	[92577188734435] = EncodeSpaces("Anime Final Quest.lua"), -- Endless3
+	[95240177479580] = EncodeSpaces("Anime Final Quest.lua"), -- DIO
+	[95996380540192] = EncodeSpaces("Anime Final Quest.lua"), -- Endless2
+	[100744519298647] = EncodeSpaces("Anime Final Quest.lua"), -- [AIZEN] Anime Final Quest (Lobby)
+	[112175864208911] = EncodeSpaces("Anime Final Quest.lua"), -- Cursed Realm Endless
+	[113644871968832] = EncodeSpaces("Anime Final Quest.lua"), -- AFK
+	[113963744934619] = EncodeSpaces("Anime Final Quest.lua"), -- Boss
+	[120826799896677] = EncodeSpaces("Anime Final Quest.lua"), -- Admin Room
+	[126889338150949] = EncodeSpaces("Anime Final Quest.lua"), -- Agris Evolve
+	[127316980657672] = EncodeSpaces("Anime Final Quest.lua"), -- Wave2
+	[129369781546936] = EncodeSpaces("Anime Final Quest.lua"), -- Manipulator
+	[130426042808804] = EncodeSpaces("Anime Final Quest.lua"), -- Cursed Realm
+	[132561514935233] = EncodeSpaces("Anime Final Quest.lua"), -- MetroStation1
+	[136683944064056] = EncodeSpaces("Anime Final Quest.lua"), -- Wave1
+	
 }
+
 
 function LoadScript(id)
 	local url = Games[id]
